3950f73053790f48a6aab484b33cd646b9315194
yancome <yancome@gmail.com>
2024-01-18 09:16:37 +0800

    VS change

--- GitTest/GitTest/GitTestTT.cpp
+++ GitTest/GitTest/GitTestTT.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main() {
     cout << "Hello World!";
     cout << "GitTest!";
+    cout << "This line add to show the staging area";
+    cout << "staging area test";
     return 0;
 }
 
